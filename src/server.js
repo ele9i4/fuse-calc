e4ae255b952d76baf93bc5f41179c1618136f6a8
Th0r <th0r@inbox.ru>
2020-01-20 14:21:51 +0500

    Add publish flow and dotenv

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import { config } from 'dotenv';
 
-const { PORT, NODE_ENV } = process.env;
+config();
+const { PORT, NODE_ENV, FOLDER } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka()
-  .use(
+  .use(FOLDER,
     compression({ threshold: 0 }),
     sirv('static', { dev }),
     sapper.middleware()
